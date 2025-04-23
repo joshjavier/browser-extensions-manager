eab48cb7bf67335881c60469f0df7e28d021277e
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-04-23 15:37:32 +0800

    create initial components

---
+++ src/components/MainContainer/MainContainer.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@mantine/core';
+
+export function MainContainer() {
+  return (
+    <Container>
+      <div>Header</div>
+      <div>Extensions Container</div>
+    </Container>
+  );
+}

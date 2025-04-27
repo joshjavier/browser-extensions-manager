450772c911357b29f8e0a9214ae03bdfb7b614ab
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-04-27 10:06:20 +0800

    memoize ExtensionCard

--- src/components/ExtensionCard/ExtensionCard.tsx
+++ src/components/ExtensionCard/ExtensionCard.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import { IconPuzzle } from '@tabler/icons-react';
 import { Avatar, Button, Flex, Group, Modal, Paper, Text, Title } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
@@ -7,49 +8,51 @@ import classes from './ExtensionCard.module.css';
 
 type ExtensionCardProps = Pick<Extension, 'id' | 'logo' | 'name' | 'description' | 'isActive'>;
 
-export function ExtensionCard({ id, logo, name, description, isActive }: ExtensionCardProps) {
-  const [opened, { open, close }] = useDisclosure(false);
+export const ExtensionCard = memo(
+  ({ id, logo, name, description, isActive }: ExtensionCardProps) => {
+    const [opened, { open, close }] = useDisclosure(false);
 
-  return (
-    <Paper className={classes.root} withBorder>
-      <Flex gap={16}>
-        <Avatar size={60} radius={10} src={logo}>
-          <IconPuzzle size={32} />
-        </Avatar>
-        <div className={classes.details}>
-          <Title order={2} fz="xl">
-            {name}
-          </Title>
-          <Text>{description}</Text>
-        </div>
-      </Flex>
+    return (
+      <Paper className={classes.root} withBorder>
+        <Flex gap={16}>
+          <Avatar size={60} radius={10} src={logo}>
+            <IconPuzzle size={32} />
+          </Avatar>
+          <div className={classes.details}>
+            <Title order={2} fz="xl">
+              {name}
+            </Title>
+            <Text>{description}</Text>
+          </div>
+        </Flex>
 
-      <Flex className={classes.actions}>
-        <>
-          <Modal
-            opened={opened}
-            onClose={close}
-            title={`Remove ${name}`}
-            classNames={{ title: classes.modalTitle }}
-            radius={20}
-            padding={20}
-          >
-            Are you sure you want to remove this extension? This action cannot be undone.
-            <Group mt="lg" justify="flex-end">
-              <Button variant="default" onClick={close}>
-                Cancel
-              </Button>
-              <Button variant="filled" onClick={() => removeExtension(id)}>
-                Remove
-              </Button>
-            </Group>
-          </Modal>
-          <Button variant="default" onClick={open}>
-            Remove
-          </Button>
-        </>
-        {isActive != null && <ActiveSwitch id={id} isActive={isActive} />}
-      </Flex>
-    </Paper>
-  );
-}
+        <Flex className={classes.actions}>
+          <>
+            <Modal
+              opened={opened}
+              onClose={close}
+              title={`Remove ${name}`}
+              classNames={{ title: classes.modalTitle }}
+              radius={20}
+              padding={20}
+            >
+              Are you sure you want to remove this extension? This action cannot be undone.
+              <Group mt="lg" justify="flex-end">
+                <Button variant="default" onClick={close}>
+                  Cancel
+                </Button>
+                <Button variant="filled" onClick={() => removeExtension(id)}>
+                  Remove
+                </Button>
+              </Group>
+            </Modal>
+            <Button variant="default" onClick={open}>
+              Remove
+            </Button>
+          </>
+          {isActive != null && <ActiveSwitch id={id} isActive={isActive} />}
+        </Flex>
+      </Paper>
+    );
+  }
+);

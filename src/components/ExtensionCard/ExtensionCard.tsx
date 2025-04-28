671c5b387f55691fa80f2d993547f426f3f14f85
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-04-28 15:32:58 +0800

    optimize confirmation modal

--- src/components/ExtensionCard/ExtensionCard.tsx
+++ src/components/ExtensionCard/ExtensionCard.tsx
@@ -1,45 +1,24 @@
 import { memo } from 'react';
-import { Button, Flex, Group, Modal, Paper } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { Extension, removeExtension } from '@/pages/Home/Home.state';
+import { Button, Flex, Paper } from '@mantine/core';
+import { Extension } from '@/pages/Home/Home.state';
 import { ActiveSwitch } from './ActiveSwitch';
 import { ExtensionInfo } from './ExtensionInfo';
 import classes from './ExtensionCard.module.css';
 
-type ExtensionCardProps = Pick<Extension, 'id' | 'logo' | 'name' | 'description' | 'isActive'>;
+type ExtensionCardProps = Pick<Extension, 'id' | 'logo' | 'name' | 'description' | 'isActive'> & {
+  onRemove: (id: number) => void;
+};
 
 export const ExtensionCard = memo(
-  ({ id, logo, name, description, isActive }: ExtensionCardProps) => {
-    const [opened, { open, close }] = useDisclosure(false);
-
+  ({ id, logo, name, description, isActive, onRemove }: ExtensionCardProps) => {
     return (
       <Paper className={classes.root} withBorder>
         <ExtensionInfo logo={logo} name={name} description={description} />
 
         <Flex className={classes.actions}>
-          <>
-            <Modal
-              opened={opened}
-              onClose={close}
-              title={`Remove ${name}`}
-              classNames={{ title: classes.modalTitle }}
-              radius={20}
-              padding={20}
-            >
-              Are you sure you want to remove this extension? This action cannot be undone.
-              <Group mt="lg" justify="flex-end">
-                <Button variant="default" onClick={close}>
-                  Cancel
-                </Button>
-                <Button variant="filled" onClick={() => removeExtension(id)}>
-                  Remove
-                </Button>
-              </Group>
-            </Modal>
-            <Button variant="default" onClick={open}>
-              Remove
-            </Button>
-          </>
+          <Button variant="default" onClick={() => onRemove(id)}>
+            Remove
+          </Button>
           {isActive != null && <ActiveSwitch id={id} isActive={isActive} />}
         </Flex>
       </Paper>
--- src/pages/Home/Home.state.ts
+++ src/pages/Home/Home.state.ts
@@ -3,16 +3,26 @@ import { proxy } from 'valtio';
 export type Status = 'active' | 'inactive';
 export type Filter = Status | 'all';
 export type Extension = {
-  id?: number;
+  id: number;
   logo?: string;
   name: string;
   description: string;
   isActive?: boolean;
 };
+export type Modal = {
+  title: string;
+  description: 'Are you sure you want to remove this extension? This action cannot be undone.';
+  extensionId: Extension['id'] | null;
+};
 
-export const state = proxy<{ filter: Filter; extensions: Extension[] }>({
+export const state = proxy<{ filter: Filter; extensions: Extension[]; modal: Modal }>({
   filter: 'all',
   extensions: [],
+  modal: {
+    title: 'Remove extension',
+    description: 'Are you sure you want to remove this extension? This action cannot be undone.',
+    extensionId: null,
+  },
 });
 
 export const toggleActive = (id: Extension['id']) => {
